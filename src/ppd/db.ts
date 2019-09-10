d34d0a87a8cc63cc9a8227e6532c0c6b318556eb
Si Gang Chen <sigang.chen@agfa.com>
2019-09-10 12:35:04 +0800

    ignore .db

--- src/ppd/db.ts
+++ src/ppd/db.ts
@@ -3,7 +3,7 @@ import * as knexInit from "knex";
 export const knex = knexInit({
   client: 'sqlite3',
   connection: {
-    filename: "../ppd.db"
+    filename: "./ppd.db"
   },
   acquireConnectionTimeout: 1000000
   // useNullAsDefault: true
